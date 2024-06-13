a9264acc2c6bf3a99fd7cedaf7c4304a1bf7f750
Saawan Patel <s463pate@uwaterloo.ca>
2024-06-13 16:58:52 -0400

    save and load model

--- networks.py
+++ networks.py
@@ -1,39 +1,11 @@
-import torch
 from torch import nn
-
-"""
-Ideas
-can use today's date and see if that effects the quality of the output
-can use actions=True for yfinance for dividends and stock splits
-
-use bilinear to make input layer smaller
-use pairwisedistance to get a day to day change
-use convolution or something to make the 6 different values into one
-
-start with simple linear layers and add different activation layers in between
-then try using bilinear
-then try implmenting pairwise to both
-
-use three years worth of data for predictions
-expirement with different output sizes (1 day vs 1 week)
-use multiple nn to get different information one for each of the following:
-    - open
-    - high
-    - low
-    - close
-    - adj close
-    - volume
-can obtain each of these to add to the list of prices for input and remove oldest price to keep same size
-then preform in a loop to obtain multiple time steps
-store trained values for different stocks for future use
-"""
 
 
 class LinearNetwork(nn.Module):
     def __init__(self, history_length, input_size, output_size):
         super().__init__()
         self.linear_stack = nn.Sequential(
-            nn.Flatten(),
+            nn.Flatten(0),
             nn.Linear(history_length * input_size, 10),
             nn.Linear(10, output_size)
         )
@@ -44,25 +16,15 @@ class LinearNetwork(nn.Module):
 
 
 class LSTM(nn.Module):
-    def __init__(self, seq_length, input_size, hidden_length):
+    def __init__(self, seq_length, input_size, hidden_length, output_size):
         super().__init__()
         self.lstm = nn.LSTM(input_size, hidden_length, batch_first=True)
         self.stack = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(seq_length * hidden_length, 1)
+            nn.Linear(seq_length * hidden_length, output_size)
         )
 
     def forward(self, x):
         out, _ = self.lstm(x)
         out = self.stack(out)
         return out
-
-
-if __name__ == '__main__':
-    rnn = nn.LSTM(10, 20, 2, batch_first=True)
-    inp = torch.ones(5, 3, 10)
-    output, (hn, cn) = rnn(inp)
-    print(output.size())
-    flatten = nn.Flatten()
-    output = flatten(output)
-    print(output.size())
--- training.py
+++ training.py
@@ -1,4 +1,4 @@
-from typing import Optional, Type
+from typing import Optional, Type, Tuple
 from datetime import date, timedelta
 import json
 import os
@@ -31,12 +31,10 @@ def get_data_from_yahoo(stock: str, out_file: Optional[str] = None, random_order
     stock_data = yf.download(stock, '2016-04-01', yesterday)
 
     in_data = []
-
     for index, d in stock_data.iterrows():
-        in_data.append([d["Open"], d["Close"], d["Open"]-d["Close"]])
+        in_data.append([d["Open"], d["Close"], d["Open"] - d["Close"], d["High"] - d["Low"]])
     in_data = scaler.fit_transform(in_data).tolist()
 
-    # use sliding window to obtain data in 1000 day intervals
     data = []
     answers = []
     for i in range(len(in_data) - HISTORY_LENGTH - PREDICTION_LENGTH):
@@ -59,13 +57,12 @@ def get_data_from_yahoo(stock: str, out_file: Optional[str] = None, random_order
         out_file = f"{stock}_data.txt"
     open(out_file, 'w').close()  # clear any old data
     with open(out_file, "w") as f:
-        l = [list(scaler.data_min_), list(scaler.data_max_), training_data, training_answers, test_data, test_answers]
-        json.dump(l, f, indent=2)
+        json.dump([list(scaler.data_min_), list(scaler.data_max_), training_data, training_answers, test_data, test_answers], f, indent=2)
 
     return scaler, training_data, training_answers, test_data, test_answers
 
 
-def train(training_data: torch.Tensor, training_answers: torch.Tensor, device: str, network: Type[torch.nn.Module], arg):
+def train(training_data: torch.Tensor, training_answers: torch.Tensor, device: str, network: Type[torch.nn.Module], arg: Tuple):
     print("Initializing model")
     model = network(*arg).to(device=device)
     model.train()
@@ -80,10 +77,9 @@ def train(training_data: torch.Tensor, training_answers: torch.Tensor, device: s
         for data, answer in zip(training_data, training_answers):
             optimizer.zero_grad()
             output = model(data)
-            a = torch.Tensor([[answer[0][0]]])
-            loss = loss_fn(output, a)
-            if i % 100 == 0:
-                print(f"Loss at {i}: {loss.item()}")
+            loss = loss_fn(output, answer[0])
+            # if i % 100 == 0:
+            #     print(f"Loss at {i}: {loss.item()}")
             loss.backward()
             optimizer.step()
             i += 1
@@ -99,11 +95,8 @@ def test(model: Type[torch.nn.Module], test_data: torch.Tensor, test_answers: to
 
     for data, answer in zip(test_data, test_answers):
         output = model(data)
-        a = torch.Tensor([[answer[0][0]]])
-        l = loss_fn(output, a)
-        loss.append(l.item())
-        e = abs((output - a)[0].item())
-        print(e)
+        loss.append(loss_fn(output, answer[0]).item())
+        e = abs((output - answer)[0][0].item())
         error.append(e)
 
     avg_loss = sum(loss) / len(loss)
@@ -112,15 +105,14 @@ def test(model: Type[torch.nn.Module], test_data: torch.Tensor, test_answers: to
     print(f"{avg_error = }")
 
 
-def run(model: Type[torch.nn.Module], scaler: MinMaxScaler, input_data: torch.Tensor, true_values: torch.Tensor):
+def graph(model: Type[torch.nn.Module], scaler: MinMaxScaler, input_data: torch.Tensor, true_values: torch.Tensor):
     scaled_predicted = []
     true_values = [i[0] for i in true_values]
     scaled_true = scaler.inverse_transform(true_values)
 
     for inp in input_data:
-        # TODO make more general
         output = model(inp)
-        output = scaler.inverse_transform([[output[0].item(), 0, 0]])[0]
+        output = scaler.inverse_transform([output.detach().numpy()])[0]
         scaled_predicted.append(list(output))
 
     true = [i[0].item() for i in scaled_true]
@@ -133,7 +125,7 @@ def run(model: Type[torch.nn.Module], scaler: MinMaxScaler, input_data: torch.Te
 
     plt.figure()
     plt.scatter(true, predicted)
-    plt.plot([0, max(predicted)], [0, max(predicted)])
+    plt.plot([0, max(predicted)], [0, max(predicted)], color="red")
     plt.show()
 
 
@@ -148,20 +140,33 @@ def main():
     print(f"Using device: {device}")
 
     s = 'AAPL'
-    if os.path.exists(f"data/{s}_data.txt"):
+    replace = True
+    if os.path.exists(f"data/{s}_data.txt" and not replace):
         print("Retrieving data from file")
         scaler, training_data, training_answers, test_data, test_answers = get_data_from_file(f"data/{s}_data.txt")
     else:
         print("Retrieving data from yfinance")
-        scaler, training_data, training_answers, test_data, test_answers = get_data_from_yahoo(s, f"data/{s}_data.txt")
+        scaler, training_data, training_answers, test_data, test_answers = get_data_from_yahoo(s, f"data/{s}_data.txt", random_order=True)
     training_data = torch.Tensor(training_data, device=device)
     training_answers = torch.Tensor(training_answers, device=device)
     test_data = torch.Tensor(test_data, device=device)
     test_answers = torch.Tensor(test_answers, device=device)
 
-    model = train(training_data, training_answers, device, LSTM, (len(training_data[0]), len(training_data[0][0]), 3))
+    file_path = f"{LinearNetwork.__name__}.txt"
+    load_from_file = True
+    if load_from_file:
+        model = torch.load(file_path)
+        model.eval()
+    else:
+        # model = train(training_data, training_answers, device, LSTM, (len(training_data[0]), len(training_data[0][0]), 10, 1))
+        model = train(training_data, training_answers, device, LinearNetwork, (len(training_data[0]), len(training_data[0][0]), len(training_data[0][0])))
+        torch.save(model, file_path)
+
+    print("Test Model on training data")
+    test(model, training_data, training_answers)
+    print("Test Model on test data")
     test(model, test_data, test_answers)
-    run(model, scaler, training_data, training_answers)
+    graph(model, scaler, training_data, training_answers)
 
 
 if __name__ == "__main__":
